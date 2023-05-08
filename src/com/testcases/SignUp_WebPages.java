51083145e7cb51cc4d5851cd138f688093064c8f
markanthony-valerio <markanthony.valerio@superfuture.world>
2023-05-08 11:48:30 +0400

    added and modified signup webpages class

--- src/com/testcases/SignUp_WebPages.java
+++ src/com/testcases/SignUp_WebPages.java
@@ -1,5 +1,13 @@
 package com.testcases;
 
-public class SignUp_WebPages {
+import org.openqa.selenium.WebDriver;
 
+public class SignUp_WebPages 
+{
+	WebDriver driver;
+	
+	public SignUp_WebPages(WebDriver driver)
+	{
+		this.driver = driver;
+	}
 }
