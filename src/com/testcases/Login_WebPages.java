6ebe069cee14fdbf8627454ce68d7e50c613f0b6
markanthony-valerio <markanthony.valerio@superfuture.world>
2023-05-08 11:19:53 +0400

    adding webpage class for login

--- src/com/testcases/Login_WebPages.java
+++ src/com/testcases/Login_WebPages.java
@@ -1,6 +1,33 @@
 package com.testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Login_WebPages 
 {
-
+	WebDriver driver;
+	
+	By username_Field = By.xpath("//*[@id=\"username\"]/div/input");
+	By password_Field = By.xpath("//*[@id=\"password\"]/div/input");
+	By login_Button = By.xpath("//*[@id=\"root\"]/div/div[2]/div/form/div[2]/button");
+	
+	public Login_WebPages(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	public void fillUsername_Field(String username)
+	{
+		driver.findElement(username_Field).sendKeys(username);
+	}
+	
+	public void fillPassword_Field(String password)
+	{
+		driver.findElement(password_Field).sendKeys(password);
+	}
+	
+	public void clickLogin_button()
+	{
+		driver.findElement(login_Button).click();
+	}
 }
